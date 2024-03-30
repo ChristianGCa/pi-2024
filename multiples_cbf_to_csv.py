da337aece095037498cafb2b8fa11946a5a7a41e
ChristianGCa <candelonichristian@gmail.com>
2024-03-30 16:38:06 -0300

    Comentando o código para facilitar o entendimento

--- multiples_cbf_to_csv.py
+++ multiples_cbf_to_csv.py
@@ -5,11 +5,20 @@ import os
 
 # Função que converte .dbf em .csv
 def convert_all_dbf_to_csv(folder_path):
+
+    # Localizando todos os arquivos .dbf no caminho da pasta especificada
     dbf_files = glob.glob(os.path.join(folder_path, '*.dbf'))
     csv_paths = []
+
     for dbf_file in dbf_files:
+
+        # Configurando o nome do arquivo .csv para converter
         csv_path = dbf_file[:-4] + ".csv"
+
+        # Criando um objeto DBF com o caminho e a codificação especificada
         dbf = DBF(dbf_file, encoding='ISO-8859-1')
+
+        # Criando um arquivo .csv e preenchendo-o
         with open(csv_path, 'w', newline='') as arq:
             writer = csv.writer(arq)
             writer.writerow(dbf.field_names)
@@ -19,4 +28,4 @@ def convert_all_dbf_to_csv(folder_path):
     return csv_paths
 
 # Chamando a função e passando o caminho da pasta contendo os arquivos .dbf
-csv_files = convert_all_dbf_to_csv('/home/christian/Documentos/q')
+csv_files = convert_all_dbf_to_csv('/caminho/da/pasta/desejada')
