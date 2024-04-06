64d8e19ddee329e1f940fb2e239d97b2c50b12a6
ChristianGCa <candelonichristian@gmail.com>
2024-04-06 11:36:41 -0300

    Criando novas tabelas com os dados

--- manipulacao_dados/a.py
+++ manipulacao_dados/a.py
@@ -5,27 +5,29 @@ import matplotlib.pyplot as plt
 
 # Diretório dos arquivos CSV
 CSV_PATCH = "/home/christian/Documentos/TESTE BIG DATA/CSV"
-COLUMNS = ['UFMUN', 'IDADEPAC']
+COLUMNS = ['UFMUN', 'IDADEPAC', 'CIDPRI']
 
 # ARQUIVOS AUXILIARES
-
-# CSV municipios
-CSV_PATCH_MUNICIPIO = "/home/christian/Documentos/TESTE BIG DATA/CSV_AUXILIAR/tb_municip.csv"
+CSV_FILE_MUNICIPIO = "/home/christian/Documentos/TESTE BIG DATA/CSV_AUXILIAR/tb_municip.csv"
 COLUMNS_MUNICIPIO = ['CO_MUNICIP', 'DS_NOME']
 
-# Carregar os arquivos
-municip_df = pd.read_csv(CSV_PATCH_MUNICIPIO, usecols=COLUMNS_MUNICIPIO)
-csv_files = glob.glob(os.path.join(CSV_PATCH, '*.csv'))
+CSV_FILE_CID = "/home/christian/Documentos/TESTE BIG DATA/CSV_AUXILIAR/S_CID.csv"
+COLUMNS_CID = ['CD_COD', 'CD_DESCR']
 
-# Concatenar os arquivos
-dfs = [pd.read_csv(file, usecols=COLUMNS) for file in csv_files]
+# Carregar os arquivos
+csv_files = glob.glob(os.path.join(CSV_PATCH, '*.csv'))
+dfs = [pd.read_csv(file, usecols=COLUMNS, encoding='ISO-8859-1') for file in csv_files]
 df = pd.concat(dfs, ignore_index=True)
 
-# Mesclar os DataFrames
-merged_df = pd.merge(df, municip_df, left_on='UFMUN', right_on='CO_MUNICIP', how='left')
+municip_df = pd.read_csv(CSV_FILE_MUNICIPIO, usecols=COLUMNS_MUNICIPIO, encoding='ISO-8859-1')
+cid_df = pd.read_csv(CSV_FILE_CID, usecols=COLUMNS_CID, encoding='ISO-8859-1')
+
+# Mesclar os três dataframes, sendo UFMUN=CO_MUNICIP e CIDPRI=CD_COD
+merged_df = df.merge(municip_df, left_on='UFMUN', right_on='CO_MUNICIP').merge(cid_df, left_on='CIDPRI', right_on='CD_COD')
 
 # Selecionar e renomear as colunas desejadas
-merged_df = merged_df[['DS_NOME', 'IDADEPAC']]
-merged_df.columns = ['Nome do Município', 'Idade']
+merged_df = merged_df[['DS_NOME', 'IDADEPAC', 'CD_DESCR']]
+merged_df.columns = ['Nome do Município', 'Idade', 'CID']
 
-print(merged_df)
+# Exibir o DataFrame com as primeiras 20 linhas
+print(merged_df.head(20))
