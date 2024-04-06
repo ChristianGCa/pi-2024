1366b1c8ef662e09b73e12aa70691bb85d6e0fbc
ChristianGCa <candelonichristian@gmail.com>
2024-04-06 20:17:34 -0300

    Criando gráficos com as doenças por municipios

--- manipulacao_dados/a.py
+++ manipulacao_dados/a.py
@@ -26,8 +26,25 @@ cid_df = pd.read_csv(CSV_FILE_CID, usecols=COLUMNS_CID, encoding='ISO-8859-1')
 merged_df = df.merge(municip_df, left_on='UFMUN', right_on='CO_MUNICIP').merge(cid_df, left_on='CIDPRI', right_on='CD_COD')
 
 # Selecionar e renomear as colunas desejadas
-merged_df = merged_df[['DS_NOME', 'IDADEPAC', 'CD_DESCR']]
-merged_df.columns = ['Nome do Município', 'Idade', 'CID']
+merged_df = merged_df[['DS_NOME', 'IDADEPAC', 'CIDPRI', 'CD_DESCR']]
+merged_df.columns = ['Municipio', 'Idade', 'CID', 'Doenca']
 
-# Exibir o DataFrame com as primeiras 20 linhas
-print(merged_df.head(20))
+# Filtrar o DataFrame para incluir apenas os casos da doença especificada
+CID_DOENCA = 'F25'  # Código CID da doença
+casos_depressao = merged_df[merged_df['CID'] == CID_DOENCA]
+
+# Contar o número de casos de doença por município
+casos_por_municipio = casos_depressao['Municipio'].value_counts()
+
+# Função para mapear códigos CID para descrições de doenças
+def get_doenca_descricao(codigo_cid):
+    return cid_df[cid_df['CD_COD'] == codigo_cid]['CD_DESCR'].iloc[0]
+
+# Plotar o gráfico de barras
+plt.figure(figsize=(15, 8))
+casos_por_municipio.plot(kind='bar')
+plt.title('Casos de: '+get_doenca_descricao(CID_DOENCA))
+plt.xlabel('Município')
+plt.ylabel('Número de Casos')
+plt.xticks(rotation=90)
+plt.show()
